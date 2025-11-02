145c4cb0feb3785107ed80fc38b44f7c1d807dbe
ozgetontu <ozge.tontu@ug.bilkent.edu.tr>
2025-11-02 23:08:36 +0300

    Initial commit

--- 06_Microservices_Users.AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.Users_API>("users-api");
-
-builder.AddProject<Projects.Locations_API>("locations-api");
-
-builder.AddProject<Projects.Gateway_API>("gateway-api");
-
-builder.Build().Run();
